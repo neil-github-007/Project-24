aad92854f6b1de14592f6d30fdbfd66bd63d5dce
neil-github-007 <74409287+neil-github-007@users.noreply.github.com>
2021-01-02 12:24:23 +0530

    Update waste.js

--- waste.js
+++ waste.js
@@ -1,21 +1,21 @@
 class Waste {
-    constructor() {
+    constructor(x, y, width, height) {
         var options = {
-            isStatic:false,
             restitution:0.3,
             friction:0.5,
             density:1.2
         }
-        this.body = Bodies.circle(50, 625, 30, options);
+        this.body = Bodies.circle(x, y, width, options);
+        this.height = height;
+        this.width = width;
         World.add(world, this.body);
     }
 
     display(){
-     
+        ellipseMode(RADIUS);     
         fill('pink');
-        ellipse(50,625,30);
-
-        
-        
+        ellipse(this.body.position.x, this.body.position.y, 15, 15);
+   
     }
+
 }

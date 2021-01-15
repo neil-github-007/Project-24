e1a5a2c497d93e1afa38d36541ebf6d9063673e1
neil-github-007 <74409287+neil-github-007@users.noreply.github.com>
2021-01-15 11:39:30 +0530

    Update waste.js

--- waste.js
+++ waste.js
@@ -3,7 +3,8 @@ class Waste {
         var options = {
             restitution:0.3,
             friction:0.5,
-            density:1.2
+            density:1.2,
+            isStatic:false
         }
         this.body = Bodies.circle(x, y, width, options);
         this.height = height;
@@ -15,7 +16,9 @@ class Waste {
         ellipseMode(RADIUS);     
         fill('pink');
         ellipse(this.body.position.x, this.body.position.y, 15, 15);
-   
+        if(keyCode === UP_ARROW) {
+            Body.applyForce(this.body, this.body.position, {x: 2, y: -3});
+        }
     }
 
 }
